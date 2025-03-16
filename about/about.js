820617cb1a0108d1d650dd0c7fb757bb882fe2c2
IsaacFeld <isaacbfeldman@gmail.com>
2025-03-16 18:46:34 +0100

    Updated about section even more

--- about/about.js
+++ about/about.js
@@ -13,11 +13,28 @@ function calculateAge(){
 }
 $('primary')[0].textContent = calculateAge()
 document.onscroll = function(event){
-    console.log(window.scrollY)
-    if(window.scrollY > 75){
-        $('.gameList')[0].classList.add('in')
-    }
-    else{
-        $('.gameList')[0].classList.remove('in')
-    }
+
 }
+$('#showGameList')[0].onclick = function(event){
+
+    $('#showGameList')[0].disabled = true
+    $('.gameList')[0].classList.toggle('in')
+    setTimeout(() => {
+        $('#showGameList')[0].disabled = false
+    }, 1000)
+
+
+}
+   
+/*
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    const element = document.getElementById('embed-iframe');
+    const options = {
+        width: '33%',
+        height: '150',
+        uri: 'spotify:track:2QdcmNlQdW8iA4JEg29cgx'
+      };
+    const callback = (EmbedController) => {};
+    IFrameAPI.createController(element, options, callback);
+  };
+  */
